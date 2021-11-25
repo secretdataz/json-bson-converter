e46f686be64d091ae65764dcf0a0ebfb01fc5a7a
Secret <secret@jittapan.app>
2021-11-25 20:35:00 +0700

    Remove debug stuffs

--- jsonbson.py
+++ jsonbson.py
@@ -14,11 +14,10 @@ from pathlib import Path
 def json_to_bson(in_file, out_file):
     f = open(in_file)
     json_data = json.loads(f.read())
-    print(json_data)
     bson_data = bson.dumps(json_data)
-    print(bson_data)
     out = open(out_file, "wb")
     out.write(bson_data)
+    print("Done")
 
 def bson_to_json(in_file, out_file):
     bson_file = open(in_file, "rb")
@@ -27,6 +26,7 @@ def bson_to_json(in_file, out_file):
     pretty_json = json.dumps(decoded_data, indent=4)
     out = open(out_file, "w")
     out.write(pretty_json)
+    print("Done")
 
 if __name__ == "__main__":
     ap = argparse.ArgumentParser()
